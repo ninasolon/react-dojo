be878af87d43bd868f61ec4c47404a52d569b546
ninasolon <ninasolon@gmail.com>
2020-02-12 15:04:00 -0300

    update repos card, init routes

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useRouteMatch,
+//   useParams
+// } from "react-router-dom";
+import "./App.css";
+
+// pages
 import Home from "./components/Home";
 import UserInfo from "./components/UserInfo";
 import UserRepos from "./components/UserRepos";
-import "./App.css";
 
 function App() {
   const [error, setError] = useState(null);
@@ -64,3 +74,56 @@ function App() {
 }
 
 export default App;
+
+// <Router>
+//   <Switch>
+//     <Route path="/">
+//       <Home />
+//     </Route>
+//     <Route path="/about">
+//       <About />
+//     </Route>
+//     <Route path="/topics">
+//       <Topics />
+//     </Route>
+//   </Switch>
+// </Router>
+
+// function Topics() {
+//   let match = useRouteMatch();
+
+//   return (
+//     <div>
+//       <h2>Topics</h2>
+
+//       <ul>
+//         <li>
+//           <Link to={`${match.url}/components`}>Components</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}/props-v-state`}>
+//             Props v. State
+//           </Link>
+//         </li>
+//       </ul>
+
+//       {/* The Topics page has its own <Switch> with more routes
+//           that build on the /topics URL path. You can think of the
+//           2nd <Route> here as an "index" page for all topics, or
+//           the page that is shown when no topic is selected */}
+//       <Switch>
+//         <Route path={`${match.path}/:topicId`}>
+//           <Topic />
+//         </Route>
+//         <Route path={match.path}>
+//           <h3>Please select a topic.</h3>
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// function Topic() {
+//   let { topicId } = useParams();
+//   return <h3>Requested topic ID: {topicId}</h3>;
+// }
--- github-app/src/components/UserRepos.js
+++ github-app/src/components/UserRepos.js
@@ -3,12 +3,23 @@ import React from "react";
 export default function UserRepos(props) {
   console.log(props.repos);
   return (
-    <div className="card">
-      <ul>
+    <div className="card repos-card">
+      <ol>
         {props.repos.map(item => {
-          return <li key={item.id}>{item.name}</li>;
+          const language = item.language ? item.language : "-";
+          return (
+            <li key={item.id} className="repos-item">
+              <a href={item.html_url}>{item.full_name}</a>
+              <p>
+                <span>Issues abertas:</span> {item.open_issues}
+              </p>
+              <p>
+                <span>Linguagem:</span> {language}
+              </p>
+            </li>
+          );
         })}
-     </ul>
+      </ol>
     </div>
   );
 }
